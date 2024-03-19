72c888f5c707dcf518ad76a2d3126e494fab5215
Mmishaaa <kkochin2528@gmail.com>
2024-03-19 18:09:14 +0300

    feat: add album logic and pass album tests

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -3,10 +3,11 @@ import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 import { validate } from 'uuid';
 import { DatabaseService } from 'src/database/database.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AlbumsService {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async getById(id: string) {
     if (!validate(id))
@@ -15,7 +16,11 @@ export class AlbumsService {
         HttpStatus.BAD_REQUEST,
       );
 
-    const album = this.databaseService.findAlbum(id);
+    const album = await this.prisma.album.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
     if (!album)
       throw new HttpException(
@@ -26,31 +31,46 @@ export class AlbumsService {
     return album;
   }
 
-  create(createAlbumDto: CreateAlbumDto) {
-    const newAlbum = this.databaseService.createAlbum(createAlbumDto);
+  async create(createAlbumDto: CreateAlbumDto) {
+    const newAlbum = await this.prisma.album.create({
+      data: {
+        ...createAlbumDto,
+      },
+    });
     return newAlbum;
   }
 
-  findAll() {
-    const albums = this.databaseService.getAllAlbums();
+  async findAll() {
+    const albums = await this.prisma.album.findMany();
     return albums;
   }
 
   async findOne(id: string) {
-    const album = await this.getById(id);
+    const album = this.getById(id);
     return album;
   }
 
   async update(id: string, updateAlbumDto: UpdateAlbumDto) {
     const album = await this.getById(id);
 
-    const updatedAlbum = this.databaseService.updateAlbum(id, updateAlbumDto);
+    const updatedAlbum = await this.prisma.album.update({
+      where: {
+        id: album.id,
+      },
+      data: {
+        ...updateAlbumDto,
+      },
+    });
     return updatedAlbum;
   }
 
   async remove(id: string) {
     const album = await this.getById(id);
 
-    this.databaseService.deleteAlbum(id);
+    await this.prisma.album.delete({
+      where: {
+        id: album.id,
+      },
+    });
   }
 }
